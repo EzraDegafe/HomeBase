b34f304176497e770c612ca2b465a906f5a092c4
ezra <ezra54311@gmail.com>
2025-12-04 17:50:08 -0500

    backup bar, consisten across gallery and audio tab

--- HomeBase/ViewControllerAudios.swift
+++ HomeBase/ViewControllerAudios.swift
@@ -7,23 +7,27 @@
 
 import UIKit
 
-class ViewControllerAudios: UIViewController {
-
+class ViewControllerAudios: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    @IBOutlet weak var tableViewAudio: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableViewAudio.delegate = self
+        tableViewAudio.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "audioCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "audioCell")
+        cell.textLabel?.text = "Recording \(indexPath.row + 1)"
+        cell.detailTextLabel?.text = "0:30" // placeholder duration
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
 
 }
